1ec1c15ad871f7529860b003a00df5bcfc8dd9ca
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-06-21 22:25:43 -0700

    create a new directory Flow Control and a file with if-else statement

---
+++ 2-FlowControl/if_else.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+  
+  int number1{55};
+  int number2{66};
+
+  bool result = number1 < number2;
+  std::cout << std::boolalpha << "result is " << result << std::endl; // result is true
+
+  std::cout << std::endl;
+  std::cout << "Free Standing IF Statement" << std::endl;
+
+  if (result)
+  {
+    std::cout << number1 << " is less than " << number2 << std::endl; // 55 is less than 66
+  }
+
+  if (!result)
+  {
+    std::cout << number1 << " is less than " << number2 << std::endl;
+  }
+
+  std::cout << std::endl;
+  std::cout << "IF-ELSE Statement" << std::endl;
+
+  if (result)
+  {
+    std::cout << number1 << " is less than " << number2 << std::endl; // 55 is less than 66
+  }
+  else
+  {
+    std::cout << number1 << " is less than " << number2 << std::endl;
+  };
+
+  std::cout << std::endl;
+  std::cout << "ELSE IF Statement" << std::endl;
+
+  return 0;
+}
