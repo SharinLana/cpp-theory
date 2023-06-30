8492fd16a5264c98897df7598369bd5482b8bf90
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-06-30 11:10:58 -0700

    finish the do-while loop

--- 3-loops/do-while.cpp
+++ 3-loops/do-while.cpp
@@ -2,6 +2,16 @@
 
 int main()
 {
+  const int COUNT{10};
+  size_t i{0};
+  unsigned int j{0};
+
+  do
+  {
+    ++j;
+    std::cout << j << ". I love C++ " << std::endl;
+    i++;
+  } while (i < COUNT);
 
   return 0;
 }
