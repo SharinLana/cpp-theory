29bc18b940fff29f6b5e07ed7d505a99d59c7c8a
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-07-19 21:36:54 -0700

    initialize an array at declaration

--- 4-Arrays/arrays.cpp
+++ 4-Arrays/arrays.cpp
@@ -7,6 +7,7 @@ int main()
 
   std::cout << std::endl;
   std::cout << "Writing data in array with a loop" << std::endl;
+  std::cout << std::endl;
 
   for (size_t i{0}; i < 10; i++)
   {
@@ -17,6 +18,19 @@ int main()
   {
     std::cout << "Scores [" << i << "] " << scores[i] << std::endl;
   }
-  
+  std::cout << std::endl;
+
+  // Initialize an array at declaration
+  std::cout << std::endl;
+  std::cout << "Initialize an array at declaration" << std::endl;
+  std::cout << std::endl;
+
+  double salaries[5]{12.4, 15.6, 17.5, 11.6, 16.9};
+
+  for (size_t i{0}; i < 5; i++)
+  {
+    std::cout << "Salary [" << i << "] " << salaries[i] << std::endl;
+  }
+
   return 0;
 }
