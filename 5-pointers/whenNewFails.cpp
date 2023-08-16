dd97f762a05778ee0ebe1bcd99602631469bd30e
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-08-16 16:46:00 -0700

    references: declaration

--- 5-pointers/whenNewFails.cpp
+++ 5-pointers/whenNewFails.cpp
@@ -3,34 +3,34 @@
 int main()
 {
   // ! In some cases, the new operator can fail, so it's better to check for such errors
-  int *p_number{new int{1000000000}};
+  // int *p_number{new int{1000000000}};
 
   // Checking with 'std::exception&' mechanism
-  for (size_t i{}; i < 1000000000000; ++i)
-  {
-    try
-    {
-      int *p_number1{new int[10000000]};
-    }
-    catch (std::exception &ex)
-    {
-      std::cout << "Caught exception : " << ex.what() << std::endl; // Caught exception : std::bad_alloc
-    }
-  }
+  // for (size_t i{}; i < 1000000000000; ++i)
+  // {
+  //   try
+  //   {
+  //     int *p_number1{new int[10000000]};
+  //   }
+  //   catch (std::exception &ex)
+  //   {
+  //     std::cout << "Caught exception : " << ex.what() << std::endl; // Caught exception : std::bad_alloc
+  //   }
+  // }
 
   // Checking with 'std::nothrow'
-  for (size_t i{}; i < 1000000000000; ++i)
-  {
-    int *p_number2{new(std::nothrow) int[10000000]};
+  // for (size_t i{}; i < 1000000000000; ++i)
+  // {
+  //   int *p_number2{new(std::nothrow) int[10000000]};
 
-    if (p_number2 == nullptr)
-    {
-      std::cout << "Memory allocation failed" << std::endl;
-    }
-    else
-    {
-      std::cout << "Memory allocation succeeded" << std::endl;
-    }
-  }
+  //   if (p_number2 == nullptr)
+  //   {
+  //     std::cout << "Memory allocation failed" << std::endl;
+  //   }
+  //   else
+  //   {
+  //     std::cout << "Memory allocation succeeded" << std::endl;
+  //   }
+  // }
   return 0;
 }
---
+++ 6-references/declaration.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+int main()
+{
+  // ! Declaration and usage
+
+  int int_value{45};
+  double double_value{13.56};
+
+  // Ways to assign a value to a reference
+  int &ref_to_int_value1{int_value};
+  int &ref_to_int_value2 = int_value;
+  double &ref_to_double_value{double_value};
+
+  std::cout << "Declaring and using references (aliases)" << std::endl;
+
+  std::cout << "int_value: " << int_value << std::endl;       // 45
+  std::cout << "double_value: " << double_value << std::endl; // 13.56
+  std::cout << std::endl;
+  std::cout << "ref_to_int_value1: " << ref_to_int_value1 << std::endl;     // 45
+  std::cout << "ref_to_int_value2: " << ref_to_int_value2 << std::endl;     // 45
+  std::cout << "ref_to_double_value: " << ref_to_double_value << std::endl; // 13.56
+  std::cout << std::endl;
+  std::cout << "&int_value: " << &int_value << std::endl;                 // 0x16bcf7074
+  std::cout << "&double_value: " << &double_value << std::endl;           // 0x16bcf7068
+  std::cout << "&ref_to_int_value1: " << &ref_to_int_value1 << std::endl; // 0x16bcf7074
+  std::cout << "&ref_to_int_value2: " << &ref_to_int_value2 << std::endl; // 0x16bcf7074
+  std::cout << std::endl;
+  std::cout << "sizeof(int): " << sizeof(int) << std::endl;                             // 4
+  std::cout << "sizeof(int&): " << sizeof(int &) << std::endl;                          // 4
+  std::cout << "sizeof(ref_to_int_value1): " << sizeof(ref_to_int_value1) << std::endl; // 4
+  std::cout << std::endl;
+
+  // ! Modifying data through references
+  ref_to_int_value1 = 55;
+
+  std::cout << "After modifying the value through a reference" << std::endl;
+  std::cout << "int_value: " << int_value << std::endl;                 // 55
+  std::cout << "ref_to_int_value1: " << ref_to_int_value1 << std::endl; // 55
+  std::cout << "ref_to_int_value2: " << ref_to_int_value2 << std::endl; // 55
+
+  return 0;
+}
